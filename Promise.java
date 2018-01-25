343fd72440c4136062004963cfdc2b7533bc88b7
androidlinked <paramananda.pradhan@yahoo.com>
2018-01-25 12:17:47 +0530

    Update Promise.java

--- Promise.java
+++ Promise.java
@@ -1,20 +1,25 @@
 /*
- * PARAMANANDA PRADHAN CONFIDENTIAL
- * Copyright (c) 2017-2018 All rights reserved.
+ * The MIT License
  *
- * The source code contained or described herein and all documents
- * related to the source code ("Material") are owned by CRAWLINK
- * Networks Pvt Ltd. No part of the Material may be used, copied,
- * reproduced, modified, published, uploaded,posted, transmitted,
- * distributed, or disclosed in any way without CRAWLINK Networks
- * Pvt Ltd. prior written permission.
+ * Copyright (c) 2017-2018 Paramananda Pradhan
  *
- * No license under any patent, copyright, trade secret or other
- * intellectual property right is granted to or conferred upon you
- * by disclosure or delivery of the Materials, either expressly, by
- * implication, inducement, estoppel or otherwise. Any license
- * under such intellectual property rights must be express and
- * approved by PARAMANANDA PRADHAN in writing.
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
  *
  */
 
